9b51b0f8221fa74bbb37d1265dfb3acbd3ffb0a1
Tatsiana Yasiuchenia <tsbarankina@gmail.com>
2021-12-09 20:10:36 +0300

    feat: started html+css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from "react";
 import './App.css';
-import Nav from "./Nav.js";
-import Technologies from "./Technologies.js"
+
 
 const App = () => {
   return (
-    <div className="App">
-      <div><Nav /></div>
-      <div><Technologies /></div>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://www.buythelogo.com/wp-content/uploads/2019/03/Tree-geometric-abstract-forest-logo-vector.jpg"></img>
+      </header>
+      <nav className="nav">
+        <a>Profile</a>
+        <a>Messages</a>
+        <a>News</a>
+        <a>Music</a>
+        <a>Settings</a>
+      </nav>
+      <div className="content">
+        <img src="https://png.pngtree.com/background/20210712/original/pngtree-modern-double-color-futuristic-neon-background-picture-image_1181573.jpg"></img>
+        <div>ava +desc</div>
+        <div>my posts
+          <div>new post</div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
